bd2b16a0b4d885462c9be5bf5d176a6f51716dfd
GiovaLuc <gldacierno@gmail.com>
2024-11-20 14:57:10 +0100

    eserc1

--- 20-11/Simple1.java
+++ 20-11/Simple1.java
@@ -1,34 +1,70 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Simple1 {
 
     public static void main(String[] args) {
         Scanner myScan = new Scanner(System.in);
+        Menu myMenu = new Menu();
+
+        myMenu.addOpt("Aggiungi Libro");
+        myMenu.addOpt("Stampa Libri");
+        myMenu.addOpt("Cerca Libro");
+        myMenu.printMenu();
 
     }
 
-    public Libro(){
+    public static class Menu{
+        ArrayList<String> opts = new ArrayList();
+
+        public void addOpt(String opt){
+            opts.add(opt);
+        }
+
+        public void printMenu(){
+            for(String opt : opts){
+                System.out.println(opt);
+            }
+        }
+    }
+    
+
+    public class Libro{
         String nomeAutore;
         int numPag;
     }
 
-    public Biblioteca(){
-        ArrayList<Libro> libri;
+    public class Biblioteca{
+        ArrayList<Libro> libri = new ArrayList();
         
-        private void addLibro(String autore, int pagine){
-            //Libro libro = new Libro;
+        public void addLibro(String autore, int pagine){
+            Libro libro = new Libro();
             libro.nomeAutore = autore;
-            libro.pagine = pagine;
+            libro.numPag = pagine;
             libri.add(libro);
         }
     
         public void printLibri(){
             for(Libro libro: libri){
-                
+                System.out.println("Autore: " + libro.nomeAutore + " Pagine: " + libro.numPag);
+            }
+        }
+    
+        public void printLibro(String nomeAutore){
+            for(Libro libro: libri){
+                if(libro.nomeAutore.equals(nomeAutore)){
+                    System.out.println("Autore: " + libro.nomeAutore + " Pagine: " + libro.numPag);
+                }
+            }
+        }
+    
+        public void printLibro(int maxPag){
+            for(Libro libro: libri){
+                if(libro.numPag < maxPag){
+                    System.out.println("Autore: " + libro.nomeAutore + " Pagine: " + libro.numPag);
+                }
             }
         }
     }
+    
 }
-
-
-
